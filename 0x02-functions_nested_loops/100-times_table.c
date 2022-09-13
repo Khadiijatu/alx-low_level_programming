e037590e6e9d2eb8d609476204cafd3460951e8d
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-13 16:32:30 -0700

    Print the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,6 +15,7 @@ void print_times_table(int n)
 	if (n > 1 && n < 16)
 	{
 		for (i = 0; i <= n; i++)
+		{
 			_putchar('0');
 			for (j = 1; j <= n; j++)
 			{
@@ -23,18 +24,25 @@ void print_times_table(int n)
 				_putchar(',');
 				_putchar(' ');
 				if (k > 99)
+				{
 					_putchar(k / 100 + '0');
 					_putchar((k % 100) / 10 + '0');
 					_putchar(k % 10 + '0');
+				}
 				else if (k < 10)
+				{
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(k + '0');
+				}
 				else
+				{
 					_putchar(' ');
 					_putchar(k / 10 + '0');
 					_putchar(k % 10 + '0');
+				}
 			}
 			_putchar('\n');
+		}
 	}
 }
