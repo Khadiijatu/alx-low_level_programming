ecaf1345522bf2b0278059f2396e7efcbd655818
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-12 21:29:26 -0700

    Print _putchar using a while loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Print _putchar, followed by a new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char word[] = "_putchar\n";
+	int i = 0;
+
+	while(i < 11)
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	return (0);
+}
