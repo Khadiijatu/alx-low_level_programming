762bf6aa9da97e1d3617de162a83c1bffa3031db
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-12 22:02:06 -0700

    Check for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Check for alphabetic character
+ * @c: int
+ *
+ * Return: 1 if c is a letter and 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'z')
+		return(1);
+	else
+		return(0);
+}
