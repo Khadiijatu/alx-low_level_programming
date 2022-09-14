8f9bc3c80e480d04a59edc53750564833213be69
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-13 17:21:46 -0700

    Print the sum of the even-valued Fibonacci numbers below 4,000,000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print the sum of the even-valued Fibonacci numbers below 4,000,000
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	long sum = 2;
+	long F1 = 1;
+	long F2 = 2;
+
+	printf("%ld, %ld, ", F1, F2);
+	for (i = F1 + F2; i <= 32; i++)
+	{
+		F2 += F1;
+		F1 = F2 - F1;
+		if (F2 % 2 == 0)
+			sum += F2;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
