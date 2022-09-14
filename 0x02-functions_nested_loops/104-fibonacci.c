f9a9069dc992d13f96a5e0ace105d04aa4c9d7ef
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-13 19:35:59 -0700

    TO-DO

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,39 +8,5 @@
 
 int main(void)
 {
-	int i;
-	long a = 1;
-	long b = 2;
-	long c = 0;
-	long d = 0;
-	long e;
-
-	printf("%ld, %ld, ", a, b);
-	for (i = 3; i <= 98; i++)
-	{
-		b = b + a;
-		a = b - a;
-		if (b / 1000000000 > 0)
-		{
-			d++;
-			b = b % 1000000000;
-		}
-		d = d + c;
-		c = d - c;
-		if (d)
-		{
-			printf("%ld", d);
-			e = b;
-			while (e < 100000000)
-			{
-				printf("0");
-				e *= 10;
-			}
-		}
-		printf("%ld", b);
-		if (i < 98)
-			printf(", ");
-	}
-	printf("\n");
-	return (0);
+	TODO
 }
