e3f08ce242bb649ec97a59e538e5191fcc22e461
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-15 15:26:09 -0700

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * more_numbers - Prints the numbers from 1 to 100
+ * main - Prints the numbers from 1 to 100
  * replacing multiples of 3 with Fizz
  * multiples of 5 with Buzz, and multiple of both with BuzzFizz
  *
