bbe404035c452d424eea37d01a404db64051cc84
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-15 14:46:05 -0700

    Use while instead of for

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,16 +8,20 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	i = 0;
+	while (i < 10)
 	{
-		for (j = 0; j <= 14; i++)
+		j = 0;
+		while (j < 15)
 		{
 			if (j >= 10)
 			{
 				_putchar(j / 10 + '0');
 			}
 			_putchar(j % 10 + '0');
+			j++;
 		}
+		i++;
 		_putchar('\n');
 	}
 }
