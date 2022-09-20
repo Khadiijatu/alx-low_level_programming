96ca185ec9f8340abaf0b16e7794dc2320495e01
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-19 18:25:16 -0700

    Replace while with for

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,14 +3,12 @@
 /**
  * _puts - Prints a string, followed by a new line, to stdout
  * @str: char
- *
- * Return: 0
  */
 
 void _puts(char *str)
 {
 	int i;
-	
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
