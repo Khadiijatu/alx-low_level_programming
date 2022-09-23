383a4bcf2d6d3ec72b147b74710c01497bfa07f2
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-22 21:28:38 -0700

    Correct Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,18 +12,24 @@ char *cap_string(char *str)
 	int i, j;
 	char sep[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')',
 		'{', '}'}
+
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] -= 32;
-		}
-		for (j = 0; j < 12; i++)
-		{
-			if (str[i] == sep[j])
+			if (i == 0)
 			{
-				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-					str[i + 1] -= 32;
+				str[i] -= 32;
+			}
+			else
+			{
+				for (j = 0; j < 13; j++)
+				{
+					if (str[i - 1] == sep[j])
+					{
+						str[i] -= 32;
+					}
+				}
 			}
 		}
 	}
