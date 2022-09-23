5a244192f9e78f62206bf91b9a6fd838946b64d2
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-23 15:03:59 -0700

    Task 8

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *leet - Encodes a string using rot13
+ * @str : char pointer
+ *
+ * Return: encoded string
+ */
+
+char *rot13(char *str)
+{
+	char *letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; letters[j] != '\0'; j++)
+		{
+			if (str[i] == letters[j])
+				str[i] = rot13[j];
+		}
+	}
+	return (str);
+}
