f6073afc1c08f2b8841bc3d1d2b4fb978a86c352
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-22 18:29:15 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *_strcat - Concatenates two strings, using at most n bytes from src
+ * @dest: char
+ * @src: char
+ * @n: int
+ *
+ * Return: pointer to the string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (*(dest +i))
+	{
+		i++;
+	}
+	j = 0;
+	while (*(src + j) && j < n)
+	{
+		*(dest + i) = *(src + j);
+		j++;
+		i++;
+	}
+	return (dest);
+}
