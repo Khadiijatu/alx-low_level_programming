617f1dd8c255216256e360b660e1571c6b9324d8
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-26 01:11:42 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * *_memset - Fills memory with a constant byte
+ * @s: char pointer
+ * @b: char
+ * @: insigned int
+ *
+ * Return: pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
