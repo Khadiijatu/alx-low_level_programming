59b637a853f91f9b9fd72fdb53985025ff9a2d9d
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-26 01:18:15 -0700

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.c"
+
+/**
+ * *_memcpy - Copies memory area
+ * @dest: char pointer
+ * @src: char pointer
+ * @n: insigned int
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
