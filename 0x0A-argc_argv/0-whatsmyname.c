885a2e959865e07400cf2c6ced2126eee8eae056
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-09-30 12:46:47 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints program's name
+ * @argc: int
+ * @argv: char pointer
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
