9fad9a9b35301c0f715af01561f110f96ae1e2bd
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-10-05 23:28:20 -0700

    Task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,82 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: char
+ *
+ * Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int j = 1;
+	int num = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+		{
+			j = -1 * j;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				num = (s[i] - '0') * j + num * 10;
+				i++;
+			}
+			break;
+		}
+		i++;
+	}
+	return (num);
+}
+
+/**
+ * _printf - Prints a string
+ * @s: char pointer
+ */
+
+void print(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		print(s + 1);
+	}
+	else
+		_putchar('\n');
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: int
+ * @argv: char type pointer
+ *
+ * Return: 0 or 98
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	if (argc != 3)
+	{
+		print("Error");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				print("Error");
+				exit(98);
+			}
+		}
+	}
+	print("%d", _atoi(argv[1]) *  _atoi(argv[2]));
+	return (0);
+}
