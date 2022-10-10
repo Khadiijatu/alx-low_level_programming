7d5429f0b514072db18d83ad95bc4e812fc9e031
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-10-09 22:29:14 -0700

    Task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * *new_dog - creates a new dog
+ * @name: pointer to char
+ * @age: float
+ * @owner: pointer to char
+ *
+ * Return: pointer, or NULL on failure
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+	int i, j, k;
+
+	i = 0;
+	while (name[i])
+		i++;
+	j = 0;
+	while (owner[j])
+		j++;
+
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
+		return (NULL);
+	(*p).name = malloc(i);
+	(*p).owner = malloc(j);
+	(*p).name[0] = name[0];
+	for (k = 1; name[k]; k++)
+	{
+		(*p).name[k] = name[k];
+	}
+	(*p).owner[0] = owner[0];
+	for (k = 1; owner[k]; k++)
+	{
+		(*p).owner[k] = owner[k];
+	}
+	(*p).age = age;
+	return (p);
+}
