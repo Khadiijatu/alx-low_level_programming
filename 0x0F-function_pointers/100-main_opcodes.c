2035174e382571388387dc6dd172df12162cdd32
Khadiijatu <khadidiatoucisse4@hotmail.com>
2022-10-14 16:15:30 -0700

    Task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the opcodes of main function
+ * @argc: int
+ * @argv: char pointer
+ *
+ * Return: 0, 1 or 2
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int a = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (a < 0)
+	{
+		atoi(argv[1]);
+		exit(2);
+	}
+	i = 0;
+	while (i < a)
+	{
+		printf("%02x", (char *)main[i]);
+		i++;
+		if (a > i)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
